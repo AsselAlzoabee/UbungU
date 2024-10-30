93f0ddee4810fa0d37365d286888257c17f447a6
Dalzo <Dalzo@10.5.44.111>
2024-10-30 19:23:28 +0100

    nur TEST von diaa, nimmt das alte

--- main/Main.java
+++ main/Main.java
@@ -1,17 +1,18 @@
 package main;
+
 // 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
 import gui.MoebelhausControl;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-	
+
 	@Override
-	public void start(Stage primaryStage) {
-		new MoebelhausControl(primaryStage);
-	}	
-	
-	public static void main(String[] args){
+	public void start(Stage primaryStag) {
+		new MoebelhausControl(primaryStag);
+	}
+
+	public static void main(String[] args) {
 		launch(args);
 	}
 }
